59d1baccd7f78f9c126255787b50988a29bd3a39
haroonbajwa <haroonbajwa99@gmail.com>
2021-08-11 18:01:02 +0500

    add to cart & cart layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect} from 'react';
 import { commerce } from './lib/commerce';
-import { Navbar, Products } from './components';
+import { Navbar, Products, Cart } from './components';
+
 
 const App = () => {
 
     const [products, setProducts] = useState([]);
+
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -12,16 +15,26 @@ const App = () => {
         setProducts(data);
     }
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
+
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, [])
-
-    console.log(products);
 
     return (
         <div>
-            <Navbar />
-            <Products products={products}/>
+            <Navbar totalItems={cart.total_items} />
+            <Products products={products} onAddToCart={handleAddToCart}/>
+            <Cart cart={cart} />
         </div>
     )
 }

d4f869180ac31d2dc16e51e5c8d0098c8b0466ce
haroonbajwa <haroonbajwa99@gmail.com>
2021-08-11 22:51:20 +0500

    routing & cart buttons functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Container, Grid, Typography, Button } from '@material-ui/core';
 import CartItem from './CartItem/CartItem';
+import { Link } from 'react-router-dom';
 
 import useStyles from './styles';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
     const classes = useStyles();
 
     const EmptyCart = () => (
-        <Typography variant='subtitle1'>Your have no items in your shopping cart. Start adding some!</Typography>
+        <Typography variant='subtitle1'>Your have no items in your shopping cart, 
+            <Link to='/' className={classes.link}>Start adding some</Link>
+        </Typography>
     )
 
     const FilledCart = () => (
@@ -16,7 +19,11 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
+                        <CartItem
+                            item={item}
+                            onUpdateCartQty={handleUpdateCartQty}
+                            onRemoveFromCart={handleRemoveFromCart}
+                        />
                     </Grid>
                 ))}
             </Grid>
@@ -25,7 +32,7 @@ const Cart = ({ cart }) => {
                     Subtotal: {cart.subtotal.formatted_with_symbol}
                 </Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart}>
                         Empty Cart
                     </Button>
                     <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">
@@ -35,15 +42,13 @@ const Cart = ({ cart }) => {
             </div>
         </>
     )
-    
-    if(!cart.line_items) return 'Loading...';
 
     return (
         <Container>
             <div className={classes.toolbar} />
             <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
 
-            {!cart.line_items.length ? <EmptyCart /> : <FilledCart /> }
+            {!cart?.line_items?.length ? <EmptyCart /> : <FilledCart /> }
 
         </Container>
     )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AppBar, Toolbar, IconButton, Menu, MenuItem, Badge, Typography } from '@material-ui/core'
 import { ShoppingCart } from '@material-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../assets/commerce.png';
 
 import useStyles from './styles';
@@ -8,23 +9,27 @@ import useStyles from './styles';
 const Navbar = ({ totalItems }) => {
 
     const classes = useStyles();
+    const location = useLocation();
 
     return (
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant="h6" className={classes.title} color="inherit">
+                    <Typography component={Link} to='/' variant="h6" className={classes.title} color="inherit">
                         <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
                         Commerce.js
                     </Typography>
                     <div className={classes.grow} />
-                    <div className={classes.button}>
-                        <IconButton aria-label='Show Cart Items' color="inherit">
+                    {location.pathname !=='/cart' && (
+
+                        <div className={classes.button}>
+                        <IconButton component={Link} to='/cart' aria-label='Show Cart Items' color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                        </div>
+                    )}
                 </Toolbar>
             </AppBar>
         </>

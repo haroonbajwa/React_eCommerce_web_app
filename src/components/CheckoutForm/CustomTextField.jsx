b1693e76a379aa987f6ebfd35178cc5e6d60c26d
haroonbajwa <haroonbajwa99@gmail.com>
2021-08-13 21:54:12 +0500

    address form all done

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { TextField, Grid} from '@material-ui/core'
 import { useFormContext, Controller } from 'react-hook-form'
 
-const FormInput = ({ name, label, required }) => {
+const FormInput = ({ name, label }) => {
 
     const { control } = useFormContext();
     
@@ -16,7 +16,7 @@ const FormInput = ({ name, label, required }) => {
                     <TextField
                         fullWidth
                         label={label}
-                        required={required}
+                        required
                     />
                 )}
             />
